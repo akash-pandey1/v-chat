7646947f61796e5eecea9b9ebc9b0c435d58794f
Akash <akashdeep9226@gmail.com>
2026-04-24 22:54:27 +0530

    fixes

--- backend/server.js
+++ backend/server.js
@@ -57,8 +57,8 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT 
 
-server.listen(PORT, '0.0.0.0', () => {
+server.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`)
 })
